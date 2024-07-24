d4a1d33eed65c384a51832a4b64b124216987b72
guptamanan24 <guptamanan24@gmail.com>
2024-07-25 01:13:51 +0530

    Socket url Updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://lets-chat-sqf9.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
